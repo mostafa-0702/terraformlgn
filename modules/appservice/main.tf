5653fb58dd05761d7e0cdb84be7abb3af232e77c
mostafals <mostafa.lagnaoui@gmail.com>
2026-05-13 14:19:20 +0100

    fix appservice main.tf

--- modules/appservice/main.tf
+++ modules/appservice/main.tf
@@ -1,10 +1,32 @@
-############# MODULE APPSERVICE ###############
-module "appservice" {
-  source = "./modules/appservice"
-
+############# APP SERVICE PLAN ############
+resource "azurerm_service_plan" "asp" {
+  name                = "asp-${var.project_name}-${var.environment}"
+  resource_group_name = var.resource_group_name
   location            = var.location
-  project_name        = var.project_name
-  environment         = var.environment
-  resource_group_name = azurerm_resource_group.rg.name
-  subnet_id           = module.networking.subnet_id
+  os_type             = "Linux"
+  sku_name            = "B1"
+
+  tags = {
+    environment = var.environment
+    project     = var.project_name
+    managed_by  = "terraform"
+  }
+}
+
+############# APP SERVICE ############
+resource "azurerm_linux_web_app" "app" {
+  name                = "app-${var.project_name}-${var.environment}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  site_config {
+    always_on = false
+  }
+
+  tags = {
+    environment = var.environment
+    project     = var.project_name
+    managed_by  = "terraform"
+  }
 }
