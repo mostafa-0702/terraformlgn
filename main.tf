57273b67ebc4ddba8126f29ad00cb169612d9c28
mostafals <mostafa.lagnaoui@gmail.com>
2026-05-13 12:01:33 +0100

    resolve merge conflicts

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
 ############# RG ###############
-<<<<<<< HEAD
-=======
-
->>>>>>> ddc5e063fb68924212be11aa973e212ef533d8ba
 resource "azurerm_resource_group" "rg" {
   name     = "rg-${var.project_name}-${var.environment}"
   location = var.location
@@ -14,7 +10,6 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
-<<<<<<< HEAD
 ############# MODULE NETWORKING ###############
 module "networking" {
   source = "./modules/networking"
@@ -34,130 +29,3 @@ module "storage" {
   environment         = var.environment
   resource_group_name = azurerm_resource_group.rg.name
 }
-=======
-
-
-########### vnet ############
-
-resource "azurerm_network_security_group" "nsg" {
-  name                = "nsg-${var.project_name}-${var.environment}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-${var.project_name}-${var.environment}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
-
-
-}
-
-resource "azurerm_subnet" "snet" {
-  name                 = "subnetttt-${var.project_name}-${var.environment}"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-
-
-
-}
-
-
-########### NSG ASSOCIATION ############
-resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
-  subnet_id                 = azurerm_subnet.snet.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-
-
-#################### Storage Account #################
-
-
-resource "azurerm_storage_account" "stg" {
-  name                     = "stg${var.project_name}${var.environment}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = var.environment
-    project     = var.project_name
-    managed_by  = "terraform"
-  }
-}
-
-########### APP SERVICE PLAN ############
-resource "azurerm_service_plan" "asp" {
-  name                = "asp-${var.project_name}-${var.environment}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  os_type             = "Linux"
-  sku_name            = "B1"
-
-  tags = {
-    environment = var.environment
-    project     = var.project_name
-    managed_by  = "terraform"
-  }
-}
-
-########### APP SERVICE ############
-resource "azurerm_linux_web_app" "app" {
-  name                = "app-${var.project_name}-${var.environment}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  service_plan_id     = azurerm_service_plan.asp.id
-
-  site_config {
-    always_on = false
-  }
-
-  tags = {
-    environment = var.environment
-    project     = var.project_name
-    managed_by  = "terraform"
-  }
-}
-
-
-########### SQL SERVER ############
-resource "azurerm_mssql_server" "sql" {
-  name                         = "sql-${var.project_name}-${var.environment}"
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
-  version                      = "12.0"
-  administrator_login          = var.sql_admin_login
-  administrator_login_password = var.sql_admin_password
-
-  tags = {
-    environment = var.environment
-    project     = var.project_name
-    managed_by  = "terraform"
-  }
-}
-
-########### SQL DATABASE ############
-resource "azurerm_mssql_database" "sqldb" {
-  name      = "sqldb-${var.project_name}-${var.environment}"
-  server_id = azurerm_mssql_server.sql.id
-  sku_name  = "Basic"
-
-  tags = {
-    environment = var.environment
-    project     = var.project_name
-    managed_by  = "terraform"
-  }
-}
-
-
-
-
-
-
-
-  
->>>>>>> ddc5e063fb68924212be11aa973e212ef533d8ba
